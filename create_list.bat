58479925db54f0a20140c3969fba7e8f7521af3c
mshankr <25092890+mshankr@users.noreply.github.com>
2025-05-06 15:01:14 +0800

    Update create_list.bat

--- create_list.bat
+++ create_list.bat
@@ -1,4 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
 
-npx npm-remote-ls --flatten <package@version> > npm_list3.txt
+:: To circumvent array truncation issue when you use --flatten flag instead of --verbose flag
+npx npm-remote-ls --verbose <package@version> > npm_list3.txt
